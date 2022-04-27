ab48b73993974bb61ebfea14a736841173790b08
Sagar Honnabindagi <37634857+Sagarfh@users.noreply.github.com>
2022-04-27 20:35:53 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+//export MONGO_URI='mongodb+srv://admin:admin@vuecrud.twdfb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+module.exports = {
+    mongoUri: 'mongodb+srv://admin:admin@vuecrud.twdfb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    PORT: process.env.PORT || 3000,
+}
